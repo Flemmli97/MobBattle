b1af68e237538fb7b2725728bee30bf3ceae598b
Flemmli97 <Flemmli97@users.noreply.github.com>
2018-05-13 20:51:06 +0200

    fixed a server crash

--- src/main/java/com/flemmli97/mobbattle/items/entitymanager/EventHandler.java
+++ src/main/java/com/flemmli97/mobbattle/items/entitymanager/EventHandler.java
@@ -120,6 +120,7 @@ public class EventHandler {
     		}
     }
     
+    @SideOnly(value=Side.CLIENT)
     private void renderBlockOutline(EntityPlayerSP player,  BlockPos pos, BlockPos pos2, float partialTicks)
     {
     		AxisAlignedBB aabb = Team.getBoundingBoxPositions(pos, pos2).contract(0.1);
