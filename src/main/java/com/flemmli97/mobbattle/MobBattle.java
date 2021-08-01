a8888129ed703d57c6af6dbf42407e18409527cc
Flemmli97 <Flemmli97@users.noreply.github.com>
2021-08-01 18:20:00 +0200

    fix #11 server crash

--- src/main/java/com/flemmli97/mobbattle/MobBattle.java
+++ src/main/java/com/flemmli97/mobbattle/MobBattle.java
@@ -1,36 +1,36 @@
 package com.flemmli97.mobbattle;
 
-import com.flemmli97.mobbattle.client.gui.MultiItemColor;
 import com.flemmli97.mobbattle.handler.ClientEvents;
 import com.flemmli97.mobbattle.handler.EventHandler;
 import com.flemmli97.mobbattle.inv.ContainerArmor;
 import com.flemmli97.mobbattle.network.PacketHandler;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.inventory.container.ContainerType;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 @Mod(value = MobBattle.MODID)
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MobBattle {
 
     public MobBattle() {
         //This is stupid... 2 files for 2 config values
         ModLoadingContext.get().registerConfig(Type.CLIENT, Config.clientSpec, MODID + "-client.toml");
         ModLoadingContext.get().registerConfig(Type.COMMON, Config.commonSpec, MODID + ".toml");
+        MinecraftForge.EVENT_BUS.register(new EventHandler());
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientEvents::register);
+        IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
+        modbus.addListener(MobBattle::preInit);
     }
 
     public static final String MODID = "mobbattle";
@@ -46,25 +46,11 @@ public class MobBattle {
     public static ContainerType<ContainerArmor> type = new ContainerType<>(
             (windowID, playerInv) -> new ContainerArmor(windowID, playerInv, null));
 
-    @SubscribeEvent
     public static void preInit(FMLCommonSetupEvent e) {
         tenshiLib = ModList.get().getModContainerById("tenshilib")
                 .map(container -> new DefaultArtifactVersion("1.16.5-1.4.0").compareTo(container.getModInfo().getVersion()) <= 0).orElse(false);
         animania = ModList.get().isLoaded("animania");
         mca = ModList.get().isLoaded("mca");
         PacketHandler.register();
-        MinecraftForge.EVENT_BUS.register(new EventHandler());
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientEvents::register);
-    }
-
-    @SubscribeEvent
-    public static void registerTextureSprite(TextureStitchEvent.Pre event) {
-        ResourceLocation res = new ResourceLocation(MobBattle.MODID, "gui/armor_slot_sword");
-        event.addSprite(res);
-    }
-
-    @SubscribeEvent
-    public static void spawnEggColor(ColorHandlerEvent.Item e) {
-        e.getItemColors().register(new MultiItemColor(), ModItems.spawner);
     }
 }
--- src/main/java/com/flemmli97/mobbattle/handler/ClientEvents.java
+++ src/main/java/com/flemmli97/mobbattle/handler/ClientEvents.java
@@ -1,6 +1,8 @@
 package com.flemmli97.mobbattle.handler;
 
+import com.flemmli97.mobbattle.MobBattle;
 import com.flemmli97.mobbattle.ModItems;
+import com.flemmli97.mobbattle.client.gui.MultiItemColor;
 import com.flemmli97.mobbattle.items.MobArmy;
 import com.flemmli97.mobbattle.items.MobEquip;
 import com.mojang.blaze3d.matrix.MatrixStack;
@@ -10,17 +12,25 @@ import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class ClientEvents {
 
     public static void register() {
         MinecraftForge.EVENT_BUS.register(new ClientEvents());
+        IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
+        modbus.addListener(ClientEvents::registerTextureSprite);
+        modbus.addListener(ClientEvents::spawnEggColor);
     }
 
     @SubscribeEvent
@@ -48,5 +58,14 @@ public class ClientEvents {
         AxisAlignedBB aabb = Utils.getBoundingBoxPositions(pos, pos2).shrink(0.1);
         WorldRenderer.drawBoundingBox(stack, buffer.getBuffer(RenderType.getLines()), aabb.grow(0.002).offset(-vec.x, -vec.y, -vec.z), 1, 0.5F, 0.5F, 1);
         buffer.finish(RenderType.LINES);
+    }
+
+    public static void registerTextureSprite(TextureStitchEvent.Pre event) {
+        ResourceLocation res = new ResourceLocation(MobBattle.MODID, "gui/armor_slot_sword");
+        event.addSprite(res);
+    }
+
+    public static void spawnEggColor(ColorHandlerEvent.Item e) {
+        e.getItemColors().register(new MultiItemColor(), ModItems.spawner);
     }
 }
