0d3a8705c290ea2697fb8a9034e94d520169f243
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-04-23 20:12:56 +0200

    increase follow range when aggro

--- common/src/main/java/io/github/flemmli97/mobbattle/handler/Utils.java
+++ common/src/main/java/io/github/flemmli97/mobbattle/handler/Utils.java
@@ -5,10 +5,14 @@ import io.github.flemmli97.mobbattle.platform.CrossPlatformStuff;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.DustParticleOptions;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.WrappedGoal;
@@ -34,6 +38,8 @@ import java.util.stream.Collectors;
 public class Utils {
 
     public static Map<ChatFormatting, DustParticleOptions> teamColor = new HashMap<>();
+
+    public static final ResourceLocation MOB_BATTLE_FOLLOW_MOD = ResourceLocation.fromNamespaceAndPath(MobBattle.MODID, "follow_target_mod");
 
     static {
         teamColor.put(ChatFormatting.AQUA, new DustParticleOptions(new Vector3f(0.01f, 0.9f, 1f), 1.0f));
@@ -145,9 +151,17 @@ public class Utils {
         if (target == null)
             return;
         setTargetTo(entity, target);
+        increaseFollow(entity);
         if (target instanceof Mob mobTarget && both) {
+            increaseFollow(mobTarget);
             setTargetTo(mobTarget, entity);
         }
+    }
+
+    private static void increaseFollow(Mob mob) {
+        AttributeInstance att = mob.getAttribute(Attributes.FOLLOW_RANGE);
+        if (att != null && !att.hasModifier(MOB_BATTLE_FOLLOW_MOD))
+            att.addTransientModifier(new AttributeModifier(MOB_BATTLE_FOLLOW_MOD, 64, AttributeModifier.Operation.ADD_VALUE));
     }
 
     private static void setTargetTo(Mob entity, LivingEntity target) {
