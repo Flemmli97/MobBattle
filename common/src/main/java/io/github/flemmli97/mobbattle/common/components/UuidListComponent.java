aeaa9457942beb368febc70c6e70071b14abe0a9
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-07-15 19:41:19 +0200

    fix list uuid (and army item) not working

--- common/src/main/java/io/github/flemmli97/mobbattle/common/components/UuidListComponent.java
+++ common/src/main/java/io/github/flemmli97/mobbattle/common/components/UuidListComponent.java
@@ -12,16 +12,14 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
-public class UuidListComponent {
+public record UuidListComponent(List<UUID> uuids) {
 
     public static final UuidListComponent EMPTY = new UuidListComponent(List.of());
 
     public static final Codec<UuidListComponent> CODEC = UUIDUtil.CODEC.listOf().fieldOf("uuids")
-            .xmap((List<UUID> t) -> new UuidListComponent(t), UuidListComponent::uuids).codec();
+            .xmap(UuidListComponent::new, UuidListComponent::uuids).codec();
     public static final StreamCodec<ByteBuf, UuidListComponent> STREAM_CODEC = UUIDUtil.STREAM_CODEC.apply(ByteBufCodecs.list())
-            .map((List<UUID> t) -> new UuidListComponent(t), UuidListComponent::uuids);
-
-    private final ImmutableList<UUID> uuids;
+            .map(UuidListComponent::new, UuidListComponent::uuids);
 
     public UuidListComponent(List<UUID> uuids) {
         this.uuids = ImmutableList.copyOf(uuids);
@@ -31,9 +29,5 @@ public class UuidListComponent {
         List<UUID> list = new ArrayList<>(this.uuids);
         cons.accept(list);
         return new UuidListComponent(list);
-    }
-
-    public ImmutableList<UUID> uuids() {
-        return this.uuids;
     }
 }
